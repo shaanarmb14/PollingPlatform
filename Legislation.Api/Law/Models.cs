bfb5096195f91cb2d751e8b3bfe398da4fda4aa5
shaanarmb14 <shaan.arora@mudbath.com.au>
2024-07-31 10:57:18 +1000

    R010 - Make name and votes nullable on UpdateLawRequest DTO

--- Legislation.Api/Law/Models.cs
+++ Legislation.Api/Law/Models.cs
@@ -2,4 +2,4 @@
 
 ///TODO: refactor?
 public record CreateLawRequest(int ReferendumID, string Name, int Votes);
-public record UpdateLawRequest(int LawID, string Name, int Votes);
+public record UpdateLawRequest(int LawID, string? Name, int? Votes);
